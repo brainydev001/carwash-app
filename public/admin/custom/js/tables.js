02722c6d6c3bb00051574e63405de61e04e737cb
Rivstein <51017849+Rivstein@users.noreply.github.com>
2022-07-06 09:42:07 +0300

    merge

--- public/admin/custom/js/tables.js
+++ public/admin/custom/js/tables.js
@@ -5,21 +5,12 @@
 /**
  *  Datatables
  */
-<<<<<<< HEAD
 (function(){
 
     $('#usersDataTable').DataTable(
         {
           autoWidth: true,
           "lengthMenu": [
-=======
-(function() {
-
-    // users
-    $('#usersDataTable').DataTable({
-        autoWidth: true,
-        "lengthMenu": [
->>>>>>> 524c50778437c0245c2e6ee4a2f792c17b09533b
             [16, 32, 64, -1],
             [16, 32, 64, "All"]
         ]
